8b545f08779506cb492804399ccaf7c31dddb225
Praveen varma <npraveen.443@gmail.com>
2016-07-13 15:45:57 +0530

    Developed a new test class for CrossBrowserTesting.

--- src/parallelTesting/crossBrowserTesting.java
+++ src/parallelTesting/crossBrowserTesting.java
@@ -1,24 +1,38 @@
 package parallelTesting;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 
+import Utils.TestReporter;
+import browserFunction.launchBrowser;
+
 public class crossBrowserTesting {
+  private WebDriver driver;
   
   @BeforeMethod
   public void beforeMethod() {
-	
+       
   }
 
-  @Test
-  public void crossBrowserTest() {
+  @SuppressWarnings("static-access")
+  @Parameters({"browserType","appURL"})
+  @Test(description="Cross-browser Testing",groups="Regression Tests")
+  public void crossBrowserTest(String browserType, String appURL) {
 	  
+	  //Pass the parameters from xml file - browserType and appURL.
+	  TestReporter.logStep("Launch the Browser and hostURL.");
+	  launchBrowser launchBrowser = new launchBrowser();
+	  launchBrowser.getBrowserType(driver, browserType, appURL);
   }
   
   @AfterMethod
   public void afterMethod() {
-	  
+	  if(driver!= null){
+		  driver.quit();
+	  }
   }
 
 }
