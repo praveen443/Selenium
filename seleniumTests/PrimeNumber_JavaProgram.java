7d422d901ac65126bd28664d2f59affb3b173112
Praveen varma <npraveen.443@gmail.com>
2016-08-12 14:47:06 +0530

    Added a new class - To print the Prime numbers between 1 and 100.

---
+++ seleniumTests/PrimeNumber_JavaProgram.java
@@ -0,0 +1,41 @@
+
+public class PrimeNumber_JavaProgram {
+
+	public static void main(String[] args){
+		
+		/*int limit = 100;
+		System.out.println("Prime numbers between 1 & "+ limit);
+		
+		for(int i=1; i<limit; i++){
+			boolean isPrime =  true;
+			for(int j=2;j<i;j++){
+				if(i % j == 0){
+					isPrime = false;
+					break;
+				}
+			}
+			if(isPrime)
+			System.out.println(i + " ");
+		}
+     }*/
+	int limit = 100;
+	
+	System.out.println("Prime numbers between 1 & " + limit);
+	
+	for(int i=1;i<limit;i++){
+		
+		boolean isPrime = true;
+		
+		for(int j=2; j<i;j++){
+			if(i % j == 0){
+				isPrime = false;
+				break;
+			}
+		}
+		if(isPrime){
+			System.out.println( i + " ");
+		}
+	}
+	
+  }
+}
